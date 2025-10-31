da0932bba6cd10b5b45e29d5401160b7c2e0b1e5
Fengze Chen <fengze1@ualberta.ca>
2025-10-31 12:36:55 -0600

    test fail hasCity not exist

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,5 +11,18 @@ public class CustomListTest {
 // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
 
-}
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        list.deleteCity(calgary);
+        //calgary should been deleted ,edmonton exist
+        assertFalse(list.hasCity(calgary));
+        assertFalse(list.hasCity(edmonton));
+
+    }
